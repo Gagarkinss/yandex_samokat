efd1d0fa716d3f3d3fb68069a6d45fdfa0eadf79
Gagarkinss <orange_fish@mail.ru>
2025-05-29 01:07:53 +0300

    Add files via upload

---
+++ yandex_samokat/create_order_request.py
@@ -0,0 +1,12 @@
+# Тестовые данные для создания заказа
+order_data = {
+    "firstName": "Вася",
+    "lastName": "Пупкин",
+    "address": "Москва, ул. Колотушкина, 10",
+    "metroStation": 4,
+    "phone": "+79991112233",
+    "rentTime": 3,
+    "deliveryDate": "2025-05-29",
+    "comment": "не звоните",
+    "color": ["BLACK"]
+}
